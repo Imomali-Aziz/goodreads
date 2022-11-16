cda0a7ee8dc799e1fbb189087b1a0d4ed93a7f85
Imomali <imomaliazizov70@gmail.com>
2022-11-16 22:24:34 +0500

    Edited to generic views

--- books/views.py
+++ books/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from books.models import Book
 
 
-class BookListView(View):
-    def get(self, request):
-        books = Book.objects.all()
+class BookListView(ListView):
+    template_name = 'book_list.html'
+    queryset = Book.objects.all()
+    context_object_name = 'books'
 
-        return render(request, 'book_list.html', {'books': books})
 
+class BookDetailView(DetailView):
+    template_name = 'book_detail.html'
+    pk_url_kwarg = 'id'
+    model = Book
 
-class BookDetailView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        return render(request, 'book_detail.html', {'book': book})
+# class BookListView(View):
+#     def get(self, request):
+#         books = Book.objects.all()
+#
+#         return render(request, 'book_list.html', {'books': books})
+
+# class BookDetailView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         return render(request, 'book_detail.html', {'book': book})
