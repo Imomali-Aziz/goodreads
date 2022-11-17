11de2a8f58fe750c20d8abf37ec0b1712c5bd48d
Imomali <imomaliazizov70@gmail.com>
2022-11-17 18:22:23 +0500

    Crispy forms and some styles

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # installed apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    # my apps
     'books',
     'users',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
